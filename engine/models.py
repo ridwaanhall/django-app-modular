101e8cf7aa5672247afc4486ce5d7ac81e2e30ef
Ridwan Halim <91854659+ridwaanhall@users.noreply.github.com>
2025-03-06 16:44:16 +0700

    refactor: remove unused files and clean up project structure

--- engine/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-class InstalledModule(models.Model):
-    name = models.CharField(max_length=255, unique=True)
-
-    def __str__(self):
-        return self.name
--- engine/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'engine'
-urlpatterns = [
-    path('module/', views.ModuleListView.as_view(), name='module_list'),
-]
