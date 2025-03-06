d5a93f9015b67907fc8e7fa4fa91b62f654b3ed8
Ridwan Halim <91854659+ridwaanhall@users.noreply.github.com>
2025-03-06 15:51:01 +0700

    feat: add product module URL routing to main application

--- modular_app/urls.py
+++ modular_app/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('engine.urls')),
+    path('product_module/', include('modules.product_module.urls')),
 ]
