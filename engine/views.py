1469d71a6d3bb898f448e09cb32cf279d27ff7c1
Ridwan Halim <91854659+ridwaanhall@users.noreply.github.com>
2025-03-06 16:37:13 +0700

    feat: implement InstalledModule model and enhance module management with confirmation prompts

--- engine/views.py
+++ engine/views.py
@@ -6,6 +6,7 @@ from django.core.management import call_command
 from django.urls import include, path
 import os
 import importlib
+from .models import InstalledModule
 
 class ModuleListView(View):
     template_name = 'engine/module_list.html'
@@ -13,41 +14,42 @@ class ModuleListView(View):
     def get(self, request, *args, **kwargs):
         module_dir = os.path.join(settings.BASE_DIR, 'modules')
         modules = [name for name in os.listdir(module_dir) if os.path.isdir(os.path.join(module_dir, name))]
-        installed_modules = [app.split('.')[-2] for app in settings.INSTALLED_APPS if app.startswith('modules.')]
+        installed_modules = list(InstalledModule.objects.values_list('name', flat=True))
         return render(request, self.template_name, {'modules': modules, 'installed_modules': installed_modules})
 
     def post(self, request, *args, **kwargs):
         action = request.POST.get('action')
         module_dir = os.path.join(settings.BASE_DIR, 'modules')
         modules = [name for name in os.listdir(module_dir) if os.path.isdir(os.path.join(module_dir, name))]
-        installed_modules = [app.split('.')[-2] for app in settings.INSTALLED_APPS if app.startswith('modules.')]
+        installed_modules = list(InstalledModule.objects.values_list('name', flat=True))
 
         if action:
             module_name = action.split('_')[1]
-            app_label = f'modules.{module_name}.apps.{module_name.capitalize()}Config'
+            app_label = f'modules.{module_name}'
+
             if action.startswith('install_'):
-                if module_name in modules and app_label not in settings.INSTALLED_APPS:
-                    settings.INSTALLED_APPS.append(app_label)
+                if module_name in modules and module_name not in installed_modules:
+                    InstalledModule.objects.create(name=module_name)
                     try:
-                        url_module = importlib.import_module(f'modules.{module_name}.urls')
-                        settings.ROOT_URLCONF.__dict__['urlpatterns'].append(
-                            path(f'{module_name}/', include(f'modules.{module_name}.urls'))
-                        )
-                        messages.success(request, f'Module {module_name} installed.')
+                        call_command('makemigrations', app_label, interactive=False)
+                        call_command('migrate', app_label, interactive=False)
+                        messages.success(request, f'Module {module_name} installed successfully. Restart the server to apply changes.')
                     except Exception as e:
-                        settings.INSTALLED_APPS.remove(app_label)
+                        InstalledModule.objects.filter(name=module_name).delete()
                         messages.error(request, f'Install failed: {str(e)}')
+
             elif action.startswith('upgrade_'):
-                if module_name in modules and app_label in settings.INSTALLED_APPS:
+                if module_name in modules and module_name in installed_modules:
                     try:
-                        call_command('makemigrations', f'modules.{module_name}', interactive=False)
-                        call_command('migrate', f'modules.{module_name}', interactive=False)
+                        call_command('makemigrations', app_label, interactive=False)
+                        call_command('migrate', app_label, interactive=False)
                         messages.success(request, f'Module {module_name} upgraded successfully.')
                     except Exception as e:
                         messages.error(request, f'Upgrade failed: {str(e)}')
-            elif action.startswith('uninstall_'):
-                if app_label in settings.INSTALLED_APPS:
-                    settings.INSTALLED_APPS.remove(app_label)
-                    messages.success(request, f'Module {module_name} uninstalled.')
 
-        return render(request, self.template_name, {'modules': modules, 'installed_modules': installed_modules})
+            elif action.startswith('uninstall_'):
+                if module_name in installed_modules:
+                    InstalledModule.objects.filter(name=module_name).delete()
+                    messages.success(request, f'Module {module_name} uninstalled successfully. Restart the server to apply changes.')
+
+        return redirect('engine:module_list')
