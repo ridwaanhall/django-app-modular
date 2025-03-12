e8c31deb4e50e76da6133d233df17e10ec156231
Ridwan Halim <91854659+ridwaanhall@users.noreply.github.com>
2025-03-12 14:45:27 +0700

    feat: add Module and Product models with basic CRUD views and URL configuration

--- modular_engine/views.py
+++ modular_engine/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Module
 
-# Create your views here.
+def module_list(request):
+    modules = Module.objects.all()
+    return render(request, 'modular_engine/module_list.html', {'modules': modules})
+
+def install_module(request, module_name):
+    module, created = Module.objects.get_or_create(name=module_name, defaults={'installed': True})
+    module.installed = True
+    module.save()
+    return redirect('module_list')
+
+def uninstall_module(request, module_name):
+    module = Module.objects.get(name=module_name)
+    module.installed = False
+    module.save()
+    return redirect('module_list')
