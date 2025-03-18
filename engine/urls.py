689d5c97066184838cb6710189e73ae214cf4d4f
Ridwan Halim <91854659+ridwaanhall@users.noreply.github.com>
2025-03-18 13:32:39 +0700

    refactor: remove unused engine and product module files and templates

--- engine/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import ModuleListView
-
-app_name = 'engine'
-
-urlpatterns = [
-    path('module/', ModuleListView.as_view(), name='module_list'),
-]
--- modules/product_module/permissions.py
+++
@@ -1,8 +0,0 @@
-ROLE_PERMISSIONS = {
-    'manager': ['create', 'read', 'update', 'delete'],
-    'user': ['create', 'read', 'update'],
-    'public': ['read'],
-}
-
-def has_permission(role, action):
-    return action in ROLE_PERMISSIONS.get(role, [])
