f316dad051aed2ce3892bc4323be43793a5235f5
Ridwan Halim <91854659+ridwaanhall@users.noreply.github.com>
2025-03-18 17:13:21 +0700

    chore: comment out WhiteNoise middleware and runserver_nostatic in settings.py

--- django_modular_project/settings.py
+++ django_modular_project/settings.py
@@ -38,7 +38,7 @@ else:
 # Application definition
 
 INSTALLED_APPS = [
-    "whitenoise.runserver_nostatic",
+    # "whitenoise.runserver_nostatic",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -52,7 +52,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    "whitenoise.middleware.WhiteNoiseMiddleware",
+    # "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
